b289411e6cd17354ce3af49fdbeae87e49e935b8
Ivanstwan <ivan.stwan@gmail.com>
2022-01-19 16:27:48 +0700

    add png, Sidebar Main done

---
+++ xtremax-frontendtest-ivansetiawan/src/JS/Component/SecondarySidebar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SecondarySidebar() {
+  return (
+    <div className="sidebar-second">
+      <div className="sidebar-second"></div>
+    </div>
+  );
+}
+
+export default SecondarySidebar;
--- xtremax-frontendtest-ivansetiawan/src/JS/Component/Sidebar.js
+++ xtremax-frontendtest-ivansetiawan/src/JS/Component/Sidebar.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../CSS/sidebar.css";
+// import png file
+import worldIcon from "../../IMG/world.png";
+import singaporeIcon from "../../IMG/singapore.png";
+import videosIcon from "../../IMG/videos.png";
+import blogIcon from "../../IMG/blog.png";
+import aboutIcon from "../../IMG/about.png";
 
 function Sidebar() {
+  const [mainChoice, setMainChoice] = useState("Browse");
+
   // Sidebar Main Choice (e.g Browse, Videos, Blog, etc)
   const sidebarMainChoice = [
-    { name: "Browse", icon: "fas fa-globe-asia" },
-    { name: "Suggest Attraction", icon: "fas fa-globe-asia" },
-    { name: "Videos", icon: "fas fa-globe-asia" },
-    { name: "Blog", icon: "fas fa-globe-asia" },
-    { name: "About", icon: "fas fa-globe-asia" },
+    { name: "Browse", icon: worldIcon },
+    { name: "Suggest Attraction", icon: singaporeIcon },
+    { name: "Videos", icon: videosIcon },
+    { name: "Blog", icon: blogIcon },
+    { name: "About", icon: aboutIcon },
   ];
 
   // Sidebar Secondary (e.g Merlion, Marina Bay Sands)
@@ -17,9 +25,37 @@ function Sidebar() {
     <div className="sidebar">
       <div className="sidebar-main">
         {sidebarMainChoice.map((data) => {
-          return (
-            <div className="sidebar-main-choice">
-              <i className={`sidebar-main-choice-icon ${data.icon}`}></i>
+          // Conditional Rendering
+          // 1. if 'Browser' chosen / active, then render with different color
+          return mainChoice === data.name ? (
+            <div
+              className="sidebar-main-choice active"
+              id={data.name}
+              onClick={(e) => {
+                console.log(e.target.id);
+                setMainChoice(e.target.id);
+              }}
+            >
+              <img
+                className="sidebar-main-choice-icon active"
+                src={data.icon}
+              />
+              <div className="sidebar-main-choice-title active">
+                {data.name}
+              </div>
+            </div>
+          ) : (
+            // 2. if not active, then render normally
+            <div
+              className="sidebar-main-choice"
+              id={data.name}
+              onClick={(e) => {
+                console.log(e.target.id);
+                setMainChoice(e.target.id);
+              }}
+            >
+              <img className="sidebar-main-choice-icon" src={data.icon} />
+
               <div className="sidebar-main-choice-title">{data.name}</div>
             </div>
           );
